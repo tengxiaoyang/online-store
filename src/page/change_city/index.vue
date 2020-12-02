83bb4c79648051533928d3dee495171eb3f2f508
txy <656877449@qq.com>
2020-12-02 11:50:58 +0800

    新增或修改

--- src/page/change_city/index.vue
+++ src/page/change_city/index.vue
@@ -1,21 +1,15 @@
 <template>
   	<div>
-    	<el-row :gutter="20">
-        <el-col :span="6"><div class="initial grid-content bg-purple"></div></el-col>
-        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+    	<el-row>
+        <el-col :span="6"><div class="initial grid-content"></div></el-col>
+        <el-col :span="6"><div class="grid-content"></div></el-col>
+        <el-col :span="6"><div class="grid-content"></div></el-col>
+        <el-col :span="6"><div class="grid-content"></div></el-col>
+        <el-col :span="6"><div class="grid-content"></div></el-col>
+        <el-col :span="6"><div class="grid-content"></div></el-col>
+        <el-col :span="6"><div class="grid-content"></div></el-col>
+        <el-col :span="6"><div class="grid-content"></div></el-col>
+        <el-col :span="6"><div class="grid-content"></div></el-col>
       </el-row>
   	</div>
 </template>
@@ -42,33 +36,10 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.el-row {
-  // margin-bottom: 20px;
-  margin: 0;
-  padding: 0;
-  &:last-child {
-    margin-bottom: 0;
-  }
-}
-.el-col {
-  padding: 0!important;
-}
-.bg-purple-dark {
-  background: #99a9bf;
-}
-.bg-purple {
-  background: #d3dce6;
-}
-.bg-purple-light {
-  background: #e5e9f2;
-}
 .grid-content {
   min-height: 40px;
   border: 1px solid #000;
-}
-.row-bg {
-  padding: 10px 0;
-  background-color: #f9fafc;
+  background: #d3dce6;
 }
 .initial {
   height: 80px;
